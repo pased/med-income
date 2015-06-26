d23f9ee0db02a52b058ab27e1b053f93fd389224
pased <pashakhin@gmail.com>
2015-06-26 17:25:03 +0600

    tidy data

--- 1-tidying.R
+++ 1-tidying.R
@@ -1,5 +1,5 @@
 # Собираем сет данных в опрятном формате
-library(dplyr)
+library(tidyr)
 
 load("./data/med_income.rda")
 load("./data/income2.rda")
@@ -10,3 +10,15 @@ income2 <- income2[match(med_income$region, income2$region),]
 
 # Делаем новую переменную mean_income в сете med_income
 med_income$mean_income <- income2$mean_income
+
+med_income <- tbl_df(med_income)
+
+data_medinc <- med_income %>%
+  gather(income, freq, -region, -mean_income, -total_n)
+
+data_medinc$region <- factor(data_medinc$region)
+data_medinc$total_n <- as.numeric(data_medinc$total_n)
+data_medinc$mean_income <- as.numeric(data_medinc$mean_income)
+data_medinc$freq <- as.numeric(data_medinc$freq)
+
+save(data_medinc, file="./data/data_medinc.rda")
