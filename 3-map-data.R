073f5e9c5164ae78dc0711c87df466b0b64db13a
pased <pashakhin@gmail.com>
2015-06-27 16:19:01 +0600

    map data fix

--- 3-map-data.R
+++ 3-map-data.R
@@ -12,4 +12,12 @@ gpclibPermit()
 russia_map <- fortify(russia, region = "ID_1")
 russia_map <- merge(russia_map, russia, by.x = "id", by.y = "ID_1")
 
+# Забайкальский край
+russia_map$NL_NAME_1 <- factor(russia_map$NL_NAME_1)
+russia_map$NL_NAME_1 <- plyr::revalue(russia_map$NL_NAME_1,
+                                      c("Читинская область"="Забайкальский край",
+                                        "Агинский Бурятский АОк"="Забайкальский край",
+                                        "Республика Северная Осетия-Алания"="Республика Северная Осетия - Алания"))
+russia_map$NL_NAME_1 <- as.character(russia_map$NL_NAME_1)
+
 save(russia_map, file="./data/russia_map.rda")
